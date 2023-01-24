12708b137f6d513ff91e230ee78f72931af2547f
sota suzuki <hurin@tatikaze.com>
2023-01-24 17:05:08 +0900

    add: test

--- lib/range_test.ts
+++ lib/range_test.ts
@@ -10,7 +10,7 @@ describe("Rangeクラス", () => {
       });
       test("下端点と上端点が同じ値である閉区間[3,3]を渡すとRangeインスタンスを返す", () => {
         const range = new Range(3, 3);
-        assert.equal(range instanceof Range, false);
+        assert.equal(range instanceof Range, true);
       });
     });
 
@@ -18,8 +18,8 @@ describe("Rangeクラス", () => {
       test("下端点が上端点を上回っている場合には'Invalid range'がスローされる", () => {
         expect(() => new Range(2, 1)).toThrowError(new Error("Invalid range"));
       });
-
-      describe("下端点もしくは上端点に整数でない値が渡された場合には'Invalid range'がスローされる", () => {
+      
+      describe("下端点もしくは上端点に整数でない値が渡された場合", () => {
         test.each([
           {
             range: [1.5, 2],
@@ -70,7 +70,7 @@ describe("Rangeクラス", () => {
           expected: false,
           description: "は上端点よりも大きいため整数閉区間に含まれない",
         },
-      ])("$input $description", ({ input, expected }) => {
+      ])("$input$description", ({ input, expected }) => {
         const range = new Range(3, 7);
         assert.equal(range.contains(input), expected);
       });
